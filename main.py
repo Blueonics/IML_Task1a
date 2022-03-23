cdee33b1c21a9ad03ef35591ff5b156c11b387a7
Blueonics <lannan.jiang@gmail.com>
2022-03-23 19:17:24 +0100

    small fix

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # Task 1a
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn import linear_model as lm
 from sklearn import model_selection as ms
 
@@ -11,10 +10,8 @@ from sklearn import model_selection as ms
 df = pd.read_csv('C:/Users/Lannan Jiang/PycharmProjects/IML_Task1/train.csv')
 
 # training data inputs
-X = df.loc[:, 'x1':'x13']
-y = df.loc[:, 'y']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
-
+X = np.asarray(df.loc[:, 'x1':'x13'])
+y = np.asarray(df.loc[:, 'y'])
 
 K = 10
 list_lamda = [0.1, 1, 10, 100, 200]
@@ -27,31 +24,13 @@ for i in range(len(list_lamda)):
     # model uses RidgeCV for leave one out
     # Note: no feature transformation
     ridge_model = lm.RidgeCV(alphas=list_lamda[i])
-    ridge_model.fit(X_train, y_train)
+    ridge_model.fit(X, y)
     # RMSE on the left-out folds
-    NMSE[i, :] = ms.cross_val_score(ridge_model, X_train, y_train, scoring='neg_mean_squared_error', cv=cv_def, n_jobs=1)
+    NMSE[i, :] = ms.cross_val_score(ridge_model, X, y, scoring='neg_mean_squared_error', cv=cv_def, n_jobs=1)
     RMSE[i] = np.sqrt(np.mean(abs(NMSE[i, :])))
-
-# linear regression
-lin_reg = lm.LinearRegression()
-lin_reg.fit(X_train, y_train)
-
-# predict on test set
-y_pred_arr = lin_reg.predict(X_test)
-
-# print("My test y ", y_test)
-# print("My predicted y ", y_pred)
-
-y_test_arr = np.asarray(y_test)
-
-loss = np.sqrt(np.mean((y_test_arr - y_pred_arr)**2))
-print("We have an RMSE between y predictions and y test data ", loss)
-
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
 
 print("RMSE corresponding to each lamda \n", RMSE)
 
 # Write to CSV
 file = 'C:/Users/Lannan Jiang/PycharmProjects/IML_Task1/submission.csv'
-pd.DataFrame(RMSE).to_csv(file, index=False, header=None)
+pd.DataFrame(RMSE).to_csv(file, index=False, header=False)
