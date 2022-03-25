faa1910da014525bc88720a50b143bbf2174f336
Blueonics <lannan.jiang@gmail.com>
2022-03-25 20:55:51 +0100

    small fix

--- main.py
+++ main.py
@@ -17,17 +17,16 @@ K = 10
 list_lamda = [0.1, 1, 10, 100, 200]
 
 NMSE = np.zeros(shape=(len(list_lamda), K))
-RMSE = np.zeros(shape=(len(list_lamda),1))
+RMSE = np.zeros(shape=(len(list_lamda), 1))
 
 for i in range(len(list_lamda)):
-    cv_def = ms.RepeatedKFold(n_splits=K, n_repeats=1, random_state=None)
+    cv_def = ms.RepeatedKFold(n_splits=K, n_repeats=1, random_state=1)
     # model uses RidgeCV for leave one out
     # Note: no feature transformation
     ridge_model = lm.RidgeCV(alphas=list_lamda[i])
-    ridge_model.fit(X, y)
     # RMSE on the left-out folds
-    NMSE[i, :] = ms.cross_val_score(ridge_model, X, y, scoring='neg_mean_squared_error', cv=cv_def, n_jobs=1)
-    RMSE[i] = np.sqrt(np.mean(abs(NMSE[i, :])))
+    NMSE[i, :] = ms.cross_val_score(ridge_model, X, y, scoring='neg_root_mean_squared_error', cv=cv_def, n_jobs=1)
+    RMSE[i] = np.mean(abs(NMSE[i, :]))
 
 print("RMSE corresponding to each lamda \n", RMSE)
 
